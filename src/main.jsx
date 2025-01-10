7260451f2bc567b75051d644242c997c5b81a83a
mohamad-taha <mohamadtahakasir@gmail.com>
2025-01-11 01:05:17 +0300

    fix some bugs

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/Blogs">
       <App />
     </BrowserRouter>
   </StrictMode>
