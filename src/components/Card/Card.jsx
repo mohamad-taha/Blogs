744f909994d0845ab3c573b600b29357c9db53ac
mohamad-taha <mohamadtahakasir@gmail.com>
2025-01-10 22:56:51 +0300

    some bugs fixed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
+import { MdArrowOutward } from "react-icons/md";
 import "./Card.css";
+
 const Card = (props) => {
   return (
     <div className="card">
       <img src={props.img} alt="card-img" />
       <small>{props.author}</small>
       <a href="#">
-        {props.link}
-        <img src="./../../assets/imgs/arrowUpRight.svg" alt="arrowUpRight" />
+        <span>{props.link}</span>
+        <MdArrowOutward fontSize={30} />
       </a>
       <p>{props.description}</p>
     </div>
